9b12feec669047f9bf62193d42b98b50c123cfc5
Krishnas-world <krishnapallan128@gmail.com>
2024-07-22 02:52:59 +0530

    routeFix

--- app/api/auth/[kindeAuth]/route.js
+++ app/api/auth/[kindeAuth]/route.js
@@ -1,6 +1,2 @@
 import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
-
-export async function GET(request, {params}) {
-  const endpoint = params.kindeAuth;
-  return await handleAuth(request, endpoint);
-}
+export const GET = handleAuth();
